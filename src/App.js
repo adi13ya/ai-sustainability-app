f411f6ed8181e19427d60f18f0ff6e3c67bdc2cb
adi13ya <aditya.gujar2023@vitstudent.ac.in>
2025-02-03 15:26:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SustainabilityScore from "./SustainabilityScore";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the AI Ambassador for Sustainability</h1>
+      <SustainabilityScore />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SustainabilityScore.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SustainabilityScore() {
+  return (
+    <div>
+      <h2>Your Sustainability Score</h2>
+      <p>Score: 85%</p>
+    </div>
+  );
+}
+
+export default SustainabilityScore;
